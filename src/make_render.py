725301aee64e4ddb4683609d03bfb40df7f22497
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-06-03 23:12:58 -0700

    👩🏿‍💻 Write result

--- src/make_render.py
+++ src/make_render.py
@@ -1,6 +1,9 @@
 import clean_enoa as ce
+import json
+
 last_year_pea = round((ce.get_trimester_pea(4) / 1_000_000), 1)
 first_trimester_pea = round((ce.get_trimester_pea(0) / 1_000_000), 1)
 to_render = {"last_year_pea" : last_year_pea, "first_trimester_pea" : first_trimester_pea}
 
-print(to_render)
+with open('docs/data.json', 'w') as f:
+    json.dump(to_render, f)
