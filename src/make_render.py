8e3035a8e7ce46bed7a5ac450d2dd8223c6ac499
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-06-04 20:26:55 -0700

    👩🏿‍💻 Add data of employed women

--- src/make_render.py
+++ src/make_render.py
@@ -1,9 +1,11 @@
 import clean_enoa as ce
 import json
 
+
 def round_millions(value):
     rounded_value = round((value / 1_000_000), 1)
     return rounded_value
+
 
 last_year_pea = round((ce.get_trimester_pea(4) / 1_000_000), 1)
 first_trimester_pea = round((ce.get_trimester_pea(0) / 1_000_000), 1)
@@ -14,6 +16,9 @@ last_year_employed_population = round((last_year_employed_population / 1_000_000
 first_trimester_employed_men = round_millions(ce.get_trimester_employed_men(0))
 last_year_employed_men = round_millions(ce.get_trimester_employed_men(4))
 difference_employed_men = last_year_employed_men - first_trimester_employed_men
+first_trimester_employed_women = round_millions(ce.get_trimester_employed_women(0))
+last_year_employed_women = round_millions(ce.get_trimester_employed_women(4))
+difference_employed_women = last_year_employed_women - first_trimester_employed_women
 to_render = {
     "last_year_pea": last_year_pea,
     "first_trimester_pea": first_trimester_pea,
@@ -21,6 +26,8 @@ to_render = {
     "first_trimester_employed_population": first_trimester_employed_population,
     "first_trimester_employed_men": first_trimester_employed_men,
     "difference_employed_men": difference_employed_men,
+    "first_trimester_employed_women": first_trimester_employed_women,
+    "difference_employed_women": difference_employed_women,
 }
 
 with open("docs/data.json", "w") as f:
